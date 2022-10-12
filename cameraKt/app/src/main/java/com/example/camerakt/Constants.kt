fba96ecfd8c37bb7a98fb9635f56d0dd485de70f
santiago osorio <santiagoosorio310@gmail.com>
2022-10-12 15:38:06 -0500

    camera app kotlin. 1st release

---
+++ cameraKt/app/src/main/java/com/example/camerakt/Constants.kt
@@ -0,0 +1,13 @@
+package com.example.camerakt
+
+import android.Manifest
+
+object Constants {
+
+    const val TAG = "camerax"
+    const val FILE_NAME_FORMAT = "yy-MM-dd-HH-mm-ss_SSS"
+    const val REQUEST_CODE_PERMISSIONS = 123
+    val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+
+
+}
